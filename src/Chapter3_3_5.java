302bd90e15e3d2b2883124b06b98c1df5efd53e8
HFrejlev <henrikfrejlev@hotmail.com>
2019-09-17 14:49:54 +0200

    Added new assignment chapter.

---
+++ src/Chapter3_3_5.java
@@ -0,0 +1,27 @@
+public class Chapter3_3_5 {
+
+
+    public static void main(String[] args) {
+
+        // 3.5.1 - Suppose x = 3 and y = 2; show the output, if any, of the following code.
+        //         What is the output if x = 3 and y = 4?
+        //         What is the output if x = 2 and y = 2?
+        int x = 3;
+        int y = 2;
+
+        if (x >2) {
+            if (y > 2) {
+                int z = x + y;
+                System.out.println("z is " + z);
+            }
+        }
+        else
+            System.out.println("x is " + z);
+
+    }
+
+
+
+
+    }
+
